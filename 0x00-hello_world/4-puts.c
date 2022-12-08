3795716794e2dd0aafc258c8893608d12fe7f9b3
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-08 04:10:00 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 void(main)
 {
-   puts("\"Programming is like building a multilingual puzzle")
-	 returs(0)
+   puts("\"Programming is like building a multilingual puzzle");
+	 returs(0);
 }
