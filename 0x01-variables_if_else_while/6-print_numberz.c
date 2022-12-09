adb22af06129dd250c4ea2e67f67788230b3ce9a
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-09 12:40:55 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *
+ *  Return:0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
