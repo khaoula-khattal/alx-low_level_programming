ef83e17994e8f9d99c905f76050e207e0b6852f4
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-09 08:38:13 -0800

    insomia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+	       	(n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
