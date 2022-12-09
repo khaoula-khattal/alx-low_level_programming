06ccdcba271370d99ecdf054f5043e07e7d9804e
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-09 13:14:07 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+
+	char a;
+
+	a = 'a';
+	n = 0;
+	while (n < 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
