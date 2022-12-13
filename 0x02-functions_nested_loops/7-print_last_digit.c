c7822b9089c62d3f6b810518cb94991d7f0adcde
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-13 12:41:23 -0800

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,12 @@
 
 int print_last_digit(int n)
 {
-	int l;
-       
-	l = n % 10;
-	if (l > 0)
-		_putchar(l + '0');
+	int l = n % 10;
+
+	if (l < 0)
+		l *= -1;
+
+	_putchar(l + '0');
+
 	return (0);
 }
