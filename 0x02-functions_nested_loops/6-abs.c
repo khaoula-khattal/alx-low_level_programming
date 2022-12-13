3c8c7fe689c671e9410a11faf6375b18dd2d2d17
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-13 12:11:37 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * -abs - compute absolute value
+ *  @v: The integer to compute
+ *  Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
