b39fc1807c1ec657ce6f2bce5ec02ed10faf1239
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-15 11:32:20 -0800

    Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,20 @@ void print_square(int size);
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i <= size; i++)
 		{
-			_putchar(35);
-		}
-		if (i != size - 1)
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			if (i == size - 1)
+			{
+				continue;
+			}
 			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
