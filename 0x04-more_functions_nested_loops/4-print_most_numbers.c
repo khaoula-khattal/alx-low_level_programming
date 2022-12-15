1946f0b433e49435d95d38cbaa34739530473403
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-15 08:32:04 -0800

    more number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - from 0 to 9
+ * no 2 no 4
+ * Return: no
+ */
+void print_most_numbers(void)
+{
+	int char c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
