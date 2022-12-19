de20cbe1b33a5d13650389769fd015070cca7c83
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-19 12:06:03 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - print every character
+ * @str: The character
+ * Return: function
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
