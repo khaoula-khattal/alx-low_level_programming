2709ec6fb72ac25feb89a22e6f3e2e729328a5ee
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-19 12:15:31 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,12 @@ void puts_half(char *str)
 	index = 0;
 	while (str[index] != '\0')
 		index++;
-	half = index / 2;
+	hlf = index / 2;
 	if (index % 2 == 1)
 		hlf++;
-	while (half < index)
+	while (hlf < index)
 	{
-		_putchar(str[half]);
+		_putchar(str[hlf]);
 		hlf++;
 	}
 	_putchar('\n');
