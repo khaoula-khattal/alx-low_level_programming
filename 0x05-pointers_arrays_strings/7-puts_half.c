1ac3e668ff35b804780bad4e4169bfd1122b0a3c
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-19 12:13:36 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print the second half of string
+ * @str: The string
+ * Return: function
+ */
+
+void puts_half(char *str)
+{
+	int index, hlf;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	half = index / 2;
+	if (index % 2 == 1)
+		hlf++;
+	while (half < index)
+	{
+		_putchar(str[half]);
+		hlf++;
+	}
+	-putchar('\n');
+}
+
