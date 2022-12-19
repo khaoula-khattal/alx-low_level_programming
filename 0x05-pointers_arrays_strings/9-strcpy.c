3644ef1995a2351233ba0202533d3424973df0a5
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-19 12:32:44 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy
+ * @dest: destiny
+ * @src: copied
+ *
+ * Return: copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
