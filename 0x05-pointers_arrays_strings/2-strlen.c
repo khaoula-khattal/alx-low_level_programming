b446c5a1da8294c6aa9026b6e1a4d0d4557e70f4
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-19 11:22:40 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of a string
+ * @s: strlen
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int index;
+	
+	for (index = 0; s[index] != '\0' ; index++);
+	return (index);
+}
