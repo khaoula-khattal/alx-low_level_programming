d750c817f250b1f3ea11dbe52addcd67c2b7452b
khaoula-khattal <khattalkhaoula1@gmail.com>
2022-12-21 13:01:56 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int a;
-	int b;
+	int index = 0, dest_len = 0;
 
-	a = 0;
-	b = 0;
-	while (dest[a++])
-	{
-		b++;
-	}
-	for (a = 0; a < n && src[a]; a++)
-	{
-		dest[b] = scr[a];
-	}
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
