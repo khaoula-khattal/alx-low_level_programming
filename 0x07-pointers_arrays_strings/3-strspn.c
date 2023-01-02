749984b01517ccd8b9a04dd07b4aee369b3ff8e5
khaoula-khattal <khattalkhaoula1@gmail.com>
2023-01-02 13:14:19 -0800

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strspn - LOcate a char
+ * @s: to be scanned
+ * @accept: match in s
+ * Return: return count 
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int count = 0;
+	char *s1, *s2;
+
+	s1 = s;
+	s2 = accept;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			if (s2[j] == s1[i])
+		{
+			count++;
+			break;
+		}
+		j++;
+		}
+	if (s[i] != accept[j])
+	{
+		break;
+	}
+	i++;
+	}
+return (count);
+}
