9754b019cd91a169e6fa8aa53d7ad7904891b35a
khaoula-khattal <khattalkhaoula1@gmail.com>
2023-01-02 12:26:33 -0800

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main."
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: char to copy
+ * @n: number of copy
+ * Return: pointer to the memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < b; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
