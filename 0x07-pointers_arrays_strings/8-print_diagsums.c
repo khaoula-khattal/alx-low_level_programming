60eae708ccfbdebcfb04ebc8781ad8e5f7892958
khaoula-khattal <khattalkhaoula1@gmail.com>
2023-01-02 13:53:07 -0800

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagsums - the sum of two
+ * @a: second array
+ * @size: number matrix
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int s1, s2;
+
+	s1 = 0;
+	s2 = 0;
+
+	i = 0;
+	while (i < size)
+	{
+		s1 = s1 + *(a + i * size +i);
+		s2 = s2 + *(a + i * size + size - i - 1);
+
+		i++;
+	}
+
+	printf("%i, %i\n", s1, s2);
+}
