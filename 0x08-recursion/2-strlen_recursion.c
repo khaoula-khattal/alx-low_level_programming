b7b049bd977589aa5318248b664393e18468e0f3
khaoula-khattal <khattalkhaoula1@gmail.com>
2023-01-04 11:53:43 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - the length
+ * @s: string
+ * Return: lenght
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1);
+}
