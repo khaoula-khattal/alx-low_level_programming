f9ec39c2bc0990eabd3fe32bbd8c09bfe6b780a8
khaoula-khattal <khattalkhaoula1@gmail.com>
2023-01-19 11:14:43 -0800

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "varidic_functions.h"
 
 /**
- * sum_them_all - calculate the sum of all its parameters 
+ * sum_them_all - calculate the sum of all its parameters
  * @n: number of arguments passed to the function
  * Return: the resulting sum
  */
