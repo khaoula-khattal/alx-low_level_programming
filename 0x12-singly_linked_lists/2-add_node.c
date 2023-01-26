4776f117c1a59bc7d22b0f2be3a647bceb341b4a
khaoula-khattal <khaoulakhattal1@gmail.com>
2023-01-26 02:59:53 -0800

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,11 +15,15 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	while (str[len])
+	{
 		len++;
+	}
 
 	new = malloc(sizeof(list_t));
 	if (!new)
+	{
 		return (NULL);
+	}
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
